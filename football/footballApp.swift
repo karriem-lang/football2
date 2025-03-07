0d38c85685f9cd2afb5cc1ae56e9ca2378f496d3
karriem-lang <karriemlang@proton.me>
2025-03-07 05:32:49 -0500

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Karriem Lang on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
